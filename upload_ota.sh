4cae354f7f499c87913e159e397a1b19d571946b
Dan Arnfield <dan@arnfield.com>
2022-11-30 09:36:08 -0600

    Replace ifconfig with ip

--- upload_ota.sh
+++ upload_ota.sh
@@ -13,6 +13,6 @@ gzip -9kf $build_dir/firmware.bin
 # python3 $tools_dir/signing.py --mode sign --privatekey ./private.key --bin $build_dir/firmware.bin.gz --out $build_dir/firmware.bin.gz.signed
 
 if [ -n "$2" ]; then
-  hostip=$(ifconfig $(route | grep default | sed -E 's/.+[0-9]\s+[0-9]\s+[0-9]\s+(.+)$/\1/') | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
+  hostip=$(ip addr show dev $(route | grep default | sed -E 's/.+[0-9]\s+[0-9]\s+[0-9]\s+(.+)$/\1/') | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
   python3 $tools_dir/espota.py -i $2 -I $hostip -f $build_dir/firmware.bin.gz
 fi
